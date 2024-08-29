8bdcdc66339f7c2c503ca4588b818100df41efd0
Jack <155328654+Jack-N-S@users.noreply.github.com>
2024-08-29 18:47:11 -0400

    create a numbered list with forEach()

--- script.js
+++ script.js
@@ -14,5 +14,9 @@ var programmingLanguages = [
 //console.log(programmingLanguages);
 
 for (var language of programmingLanguages) {
-  console.log(`I want to learn ${language}.`);
+  //console.log(`I want to learn ${language}.`);
 };
+
+programmingLanguages.forEach(function (language, index) {
+  console.log(`${index + 1}. ${language.toUpperCase()}`);
+});
