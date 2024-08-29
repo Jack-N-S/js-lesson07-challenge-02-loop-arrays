e5224570f5162358dea5d58926eaf084ad74b8ca
Jack <155328654+Jack-N-S@users.noreply.github.com>
2024-08-28 20:55:56 -0400

    First commit

---
+++ script.js
@@ -0,0 +1,18 @@
+var programmingLanguages = [
+  "Scala",
+  "Java",
+  "Go",
+  "Swift",
+  "Cobol",
+  "JavaScript",
+  "Python",
+  "Ruby",
+  "Elm",
+  "Rust",
+  "C#"
+];
+//console.log(programmingLanguages);
+
+for (var language of programmingLanguages) {
+  console.log(`I want to learn ${language}.`);
+};
